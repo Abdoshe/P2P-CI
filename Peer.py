7b9f27bb10d8fb8598c4e6617937bcc6856c4388
Pavneet Singh Anand <panand4@ncsu.edu>
2018-04-12 12:04:32 -0400

    Client and Server changes v2 beta

--- Peer.py
+++ Peer.py
@@ -1,41 +1,191 @@
+from socket import *
+import os
+import threading
+import sys
+from multiprocessing import Process # Doubt
+import time
+import platform
+import datetime
+
 class ActivePeer:
-    def __init__(self,hostName='None',uploadPort='None'):
-        self.hostname=hostName
-        self.uploadPort=uploadPort#port number where upload server of the port is listening.
+    def __init__(self,hostname='None',upload_port='None'):
+        self.hostname=hostname
+        self.upload_port = upload_port
         
     def __str__(self):
-        return 'Hostname is '+str(self.hostname) +' upload port is :'+str(self.uploadPort)
+        return 'Hostname is '+ str(self.hostname) +' upload port is :' + str(self.upload_port)
+
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return self.hostName == other.hostName and self.uploadPort == other.uploadPort
+            return self.hostname == other.hostname and self.upload_port == other.upload_port
         return False
     
 class RFC:
-    def __init__(self,rfcNumber='None',rfcTitle='None'):
-        self.rfcNumber=rfcNumber
-        self.rfcTitle=rfcTitle
-        self.rfcActivePeer=ActivePeer()
+    def __init__(self,rfc_number='None',rfc_title='None'):
+        self.rfc_number = rfc_number
+        self.rfc_title = rfc_title
+        self.rfc_active_peer = ActivePeer()
     
     def __str__(self):
-        return 'RFC '+str(self.rfcNumber) +' '+str(self.rfcTitle)+' '+str(self.rfcActivePeer.hostname)+' '+str(self.rfcActivePeer.uploadPort)
+        return 'RFC ' + str(self.rfc_number) + ' ' + str(self.rfc_title) + ' ' + str(self.rfc_active_peer.hostname) + ' ' + str(self.rfc_active_peer.upload_port)
     
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return self.rfcNumber == other.rfcNumber and self.rfcTitle == other.rfcTitle and self.rfcActivePeer == other.rfcActivePeer
+            return self.rfc_number == other.rfc_number and self.rfc_title == other.rfc_title and self.rfc_active_peer == other.rfc_active_peer
         return False
     
     
-def peer_download(downloadSocket, rfcNumber,hostName):
-    if rfcNumber is not None:
-        rfcNumber = raw_input('Enter the RFC number to be downloaded: ')
-        message = 'GET RFC ' + rfcnumber + ' P2P-CI/1.0\nHost: ' + str(hostname) + '\nOS: ' + platform.system() + ' ' + platform.release()
-    downloadsocket.send(message)
-    data = downloadsocket.recv(1024)
-    data = downloadsocket.recv(1024)
+def peer_download(peer_socket,rfc_number,hostname):
+    if rfc_number is not None:
+        rfc_number = raw_input('Enter RFC number to be downloaded: ')
+        msg = 'GET RFC ' + rfc_number + ' P2P-CI/1.0\nHost: ' + str(hostname) + '\nOS: ' + platform.system() + ' ' + platform.release()
+    peer_socket.send(msg)
+    data = peer_socket.recv(1024)
     while data:
         print data
-        data = downloadsocket.recv(1024)
-    downloadsocket.close()
-    #print 'Enter your choice:'
-    return 0
-    
+        data = peer_socket.recv(1024)
+    peer_socket.close()
+
+def peer_server():
+	peer_socket = socket(AF_INET,SOCK_STREAM)
+	peer_ip = ''
+	peer_port = int(upload_port)
+	try:
+		peer_socket.bind((peer_ip,peer_port))
+	except error, msg:
+		print 'Binding of socket to given ip, port failed. Error Code: ' + str(msg[0]) + ' Message ' + str(msg[1])
+		sys.exit()
+	try:
+		while True:
+			peer_socket.listen(3)
+			(conn,socket_info) = peer_socket.accept()
+			print '\nConnection initialized on port : ',socket_info[1]
+			peer_thread = threading.Thread(target = peerThreadHandler, args = (conn))
+			peer_thread.start()
+		peer_socket.close()
+	except KeyboardInterrupt:
+		peer_socket.close()
+		sys.exit(0)
+
+def peerThreadHandler(peer_socket):
+	response = peer_socket.recv(1024)
+	arr = response.split(' ')
+	filename = 'RFC' + arr[2] + '.txt'
+	cwd = os.getcwd()
+	files = os.listdir(cwd)
+	if filename in files:
+		last_modified = time.strftime("%a, %d %b %Y %H:%M:%S ",time.gmtime(os.path.getmtime(filename))) + 'GMT\n'
+		OS = platform.system() + ' ' + platform.release()
+		curr_time = time.strftime("%a, %d %b %Y %H:%M:%S",time.gmtime()) + 'GMT\n'
+		file_size = os.path.getsize(filename)
+		msg = 'P2P-CI/1.0 200 OK\nDate: ' + str(curr_time) +'\nOS: ' + str(OS) + '\nLast-Modified: ' + str(last_modified) + '\nContent-Length: ' + str(file_size) + '\nContent-Type: text/text\n'
+		peer_socket.send(msg)
+		file_handler = open(filename,"r")
+		data = file_handler.read(1024)
+		while data:
+			peer_socket.send(data)
+			data = file_handler.read(1024)
+		file_handler.close()
+	peer_socket.close()
+	sys.exit(0)
+
+def add_padding(msg):
+	length = len(msg)
+	while length < 1024:
+		msg += '!'
+		length -= 1
+	return msg
+
+def addRFC(clientsocket,rfc_number=None,rfc_title=None):
+	global hostname
+	hostname = gethostname()
+	if rfc_title is None:
+		rfc_number = raw_input('Enter the RFC number: ')
+		rfc_title = raw_input('Enter the RFC title : ')
+	msg = 'ADD RFC ' + str(rfc_number) + ' P2P-CI/1.0\nHOST: ' + str(hostname) + '\nPort: ' + str(upload_port) + '\nTitle: ' + str(rfc_title)
+	send_receive(msg,clientsocket)
+
+def ListRFC(clientsocket):
+    global hostname
+    hostname = gethostname()
+    msg = 'LIST ALL P2P-CI/1.0\nHOST: ' + str(hostname) + '\nPort: ' + str(upload_port) + '\n'
+    send_receive(msg,clientsocket)
+
+def LookupRFC(clientsocket):
+	global hostname
+	hostname = gethostname()
+	rfc_number = raw_input('Enter the RFC number: ')
+	rfc_title = raw_input('Enter the RFC title: ')
+	msg = 'LOOKUP RFC ' + str(rfc_number) + ' P2P-CI/1.0\nHost: ' + str(hostname) + '\nPort: ' + str(upload_port) + '\nTitle: ' + str(rfc_title)
+	send_receive(msg,clientsocket)
+
+def deletePeer(clientsocket):
+	global hostname
+	hostname = gethostname()
+	msg = 'DEL PEER P2P-CI/1.0\nHOST: ' + str(hostname) + '\nPort: ' + str(upload_port)
+	send_receive(msg,clientsocket)
+
+def send_receive(msg,clientsocket):
+    msg = add_padding(msg)
+    clientsocket.send(msg)
+    response = clientsocket.recv(1024)
+    i = 0
+    for i in xrange(len(response)):
+	    if response[i] == '!':
+            break
+    response = response[:i]
+    print '\nResponse is\n' + str(response)
+
+def menu():
+	print '************Select option*******************'
+	print '1. Add RFC'
+	print '2. List RFCs'
+	print '3. Lookup RFC'
+	print '4. Download RFC'
+	print '5. Exit( Press Ctrl+Z after getting OK status)'
+        return raw_input('Enter your choice:')
+
+def connect_to_server():
+	global servername
+	servername = raw_input('Enter the server IP: ')
+	client_socket = socket(AF_INET, SOCK_STREAM)
+	client_socket.connect((servername,serverport))
+	addRFC(client_socket,123,'A Proferred Official ICP')
+	while True:
+		selection = menu()
+		if selection == '1':
+			addRFC(client_socket)
+		elif selection == '2':
+			ListRFC(client_socket)
+		elif selection == '3':
+			LookupRFC(client_socket)
+		elif selection == '4':
+			peer_name = raw_input('Enter hostname of peer server: ')
+			peer_port = raw_input('Enter upload port of peer: ')
+			peer_socket = socket(AF_INET, SOCK_STREAM)
+			peer_socket.connect((peer_name,int(peer_port)))
+			peer_download(peer_socket,peer_name)
+		elif selection == '5':
+			deletePeer(client_socket)
+			break
+		else:
+			print 'Invalid Input'
+
+def main():
+	global upload_port
+	lock = threading.Lock()
+	upload_port = raw_input('Enter the upload port: ')
+	try:
+		peer_server_thread = threading.Thread(name = 'Peer_server_thread',target = peer_server)
+		peer_server_thread.setDaemon(True)
+		peer_server_thread.start()
+		connect_server_thread = threading.Thread(name = 'server_connect_thread',target = connect_to_server)
+		connect_server_thread.setDaemon(True)
+		connect_server_thread.start()
+		connect_server_thread.join()
+		peer_server_thread.join()
+	except KeyboardInterrupt:
+		sys.exit(0)
+
+if __name__ == '__main__':
+	main()
--- Server.py
+++ Server.py
@@ -1,176 +1,151 @@
+from socket import *
+import sys
+import os
+import threading
 
-hostName="mainServer"
-hostPort=7734
-#think do we really need active peers list? if we are storing them in the RFC list? coz that is what we search for
-
-activePeers=[]
-activeRFCs=[]
+active_peers=[]
+active_RFCs=[]
+hostname="centralServer"
+port=7734
 
 def main():
-    serversocket = socket.socket(
-    socket.AF_INET, socket.SOCK_STREAM) # AF_INET refers to ipv4, SOCK_STREAM refers to implies TCP
-    serversocket.connect((hostName, hostPort))
-    serversocket.listen(5)## of connection requests it could listen to? shouldn't this number be like 100
+    # AF_INET -> ipv4, SOCK_STREAM -> TCP
+    server_socket = socket.socket(AF_INET, SOCK_STREAM)
+    try:
+    	server_socket.bind((hostname,port))
+    except error, msg:
+		print 'Binding to local address unsuccessful. Error Code: ' + str(msg[0]) + ' Message ' + str(msg[1])
+	    sys.exit()
+    try:
+		while True:
+			server_socket.listen(50)
+            # Accepting connections
+			(conn,socket_info) = server_socket.accept()
+            # Spawning thread
+			server_thread = threading.Thread(target = peerThreadHandler, args = (conn))
+		    server_thread.start()
+	except KeyboardInterrupt:
+		server_socket.close()
+		sys.exit(0)
 
-    #dispatching thread for each client connection    
-    while 1:
-        #accept connections from outside
-        (clientsocket, address) = serversocket.accept()
-        #now do something with the clientsocket
-        #in this case, we'll pretend this is a threaded server
-        ct = client_thread(clientsocket)
-        ct.run()#run or start?
-    
-    serversocket.close()
-    
 if __name__ == '__main__':
     main()    
     
 class ActivePeer:
-    def __init__(self,hostName='None',uploadPort='None'):
-        self.hostname=hostName
-        self.uploadPort=uploadPort#port number where upload server of the port is listening.
+    def __init__(self,hostname='None',upload_port='None'):
+        self.hostname = hostname
+        self.upload_port = upload_port#port number where upload server of the port is listening.
         
     def __str__(self):
-        return 'Hostname is '+str(self.hostname) +' upload port is :'+str(self.uploadPort)
+        return 'Hostname is ' + str(self.hostname) + ' upload port is :' + str(self.upload_port)
+
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return self.hostName == other.hostName and self.uploadPort == other.uploadPort
+            return self.hostname == other.hostname and self.upload_port == other.upload_port
         return False
     
 class RFC:
-    def __init__(self,rfcNumber='None',rfcTitle='None'):
-        self.rfcNumber=rfcNumber
-        self.rfcTitle=rfcTitle
-        self.rfcActivePeer=ActivePeer()
+    def __init__(self,rfc_number='None',rfc_title='None'):
+        self.rfc_number = rfc_number
+        self.rfc_title = rfc_title
+        self.rfc_active_peer = ActivePeer()
     
     def __str__(self):
-        return 'RFC '+str(self.rfcNumber) +' '+str(self.rfcTitle)+' '+str(self.rfcActivePeer.hostname)+' '+str(self.rfcActivePeer.uploadPort)
+        return 'RFC '+str(self.rfc_number) +' '+str(self.rfc_title)+' '+str(self.rfc_active_peer.hostname)+' '+str(self.rfc_active_peer.upload_port)
     
     def __eq__(self, other):
         if isinstance(self, other.__class__):
-            return self.rfcNumber == other.rfcNumber and self.rfcTitle == other.rfcTitle and self.rfcActivePeer == other.rfcActivePeer
+            return self.rfc_number == other.rfc_number and self.rfc_title == other.rfc_title and self.rfc_active_peer == other.rfc_active_peer
         return False
     
-def peerThreadHandler(peerSocket):
+def peerThreadHandler(peer_socket):
     try:
         while True:
-            response=peerSocket.recv(1024)
+            response = peer_socket.recv(1024)
             if len(response)==0:
-                peerSocket.close()
+                peer_socket.close()
                 return
-            i=0
+            i = 0
             for i in xrange(len(response)):
                 if response[i] == '!':
                     break
-            #remove after testing
-            output = '-----------------------------------------------------------\nThe Client has requested the following:\n'
-            new_response = response[:i]
-            output = output + response[:i]
-            
-            split_response=new_response.split();
-            if split_response[0]=='ADD':
-                addRFC(new_response,peerSocket)
-            elif split_response[0]=='LOOKUP':
-                lookUpRFC(new_response,peerSocket)
-            elif split_response[0]=='LIST':
-                listRFC(new_response,peerSocket)
-            elif split_response[0]=='DEL':
-                deleteRFC(new_response,peerSocket)
+            response = response[:i]
+            arr = response.split(' ');
+            action = arr[0]
+            if action=='ADD':
+                addRFC(response,peer_socket)
+            elif action=='LOOKUP':
+                lookUpRFC(response,peer_socket)
+            elif action=='LIST':
+                listRFC(response,peer_socket)
+            elif action=='DEL':
+                deleteRFC(response,peer_socket)
     except KeyboardInterrupt:
-        serversocket.close()
+        server_socket.close()
         sys.exit(0)
         
-def concat_message(message):
-    finalLength =len(message)
-    while finalLength<1024:
-        message+='!'
-        finalLength+=1
-    return message
+def add_padding(msg):
+    length = len(msg)
+    while length < 1024:
+        msg += '!'
+        length += 1
+    return msg
 
-def addRFC(new_reponse,peerSocket):
-    split_response=new_response.split();
-    
-    rfcNumber = split_response[2]
-    hostName = split_response[5]
-    uploadPort = split_response[7]
-    title1= split_response[9:]
-    title = ' '.join(title1)
+def addRFC(reponse,peer_socket):
+    arr = response.split(' ');
+    rfc_number = arr[2]
+    hostname = arr[5]
+    upload_port = arr[7]
+    title = arr[9:]
+    title = ' '.join(title)
     #peer is already in the active peer list?
-    newPeer=ActivePeer(hostName,uploadPort)
-    
-    if newPeer not in activePeers:
-        activePeers.append(newPeer)
-    newRFC=RFC(rfcNumber,title,newPeer)
-    if newRFC not in activeRFCs:
-        activeRFCs.append(newRFC)
-    
-    peerMessage = 'RFC '+str(rfcNumber)+' '+str(title1)+' '+str(hostName)+' '+str(uploadPort)
-    peerMessage = 'P2P-CI/1.0 200 OK\n' + str(peerMessage)
-    peerMessageResponse = concat_message(peerMessage)
-    peerSocket.send(peerMessageResponse)
-    return 0
+    peer = ActivePeer(hostname,upload_port)
+    if peer not in active_peers:
+        active_peers.append(peer)
+    RFC = RFC(rfc_number,title,peer)
+    if RFC not in active_RFCs:
+        active_RFCs.append(RFC)
+    msg = 'P2P-CI/1.0 200 OK\n' + 'RFC '+str(rfc_number) + ' ' + str(title) + ' '+str(hostname) + ' ' + str(upload_port)
+    msg = add_padding(msg)
+    peer_socket.send(msg)
 
-def lookUp(new_reponse,peerSocket):
-    split_response=new_response.split();
-    rfcNumber = split_response[2]
-    title1= split_response[9:]
-    title = ' '.join(title1)
-    count=0
-    for activeRFC in activeRFCs:
-        if activeRFC.rfcNumber==rfcNumber and activeRFC.rfcTitle==title :
-            if count==0:
-                message='P2P-CI/1.0 200 OK\n'
-            message = message + 'RFC '+str(activeRFC.rfcNumber)+' '+str(activeRFC.rfcTitle)+' '+str(activeRFC.activePeer.hostName)+' '+str(activeRFC.activePeer.uploadPort)
-            count+=1;
-        if count > 0:
-            peerMessageResponse = concat_message(message)
-            peerSocket.send(peerMessageResponse)
-        if count == 0:
-            message = 'P2P-CI/1.0 404 NOT FOUND'
-            peerMessageResponse = concat_message(message)
-            peerSocket.send(peerMessageResponse)
-    
-def list(peerSocket):
-    code = 0
-    status = ''
-    rfclist = []
-    if len(activeRFCs) == 0:
-        message = 'P2P-CI/1.0 404 NOT FOUND'
-        peerMessageResponse = concat_message(message)
-        peerSocket.send(peerMessageResponse) 
-    else:
-        counter=0
-        message = 'P2P-CI/1.0 200 OK \n'
-        for activeRFC in activeRFCs:
-           message = message + str(rfc)+ '\n'
-           counter+=1
-           
-        if counter >0 :
-            peerMessageResponse = concat_message(message)
-            peerSocket.send(peerMessageResponse)
-        elif counter ==0:
-            message = 'P2P-CI/1.0 404 NOT FOUND'
-            peerMessageResponse = concat_message(message)
-            peerSocket.send(new_message)
-    return 0
+def lookUp(response,peer_socket):
+    arr = response.split(' ');
+    rfc_number = arr[2]
+    title = arr[9:]
+    title = ' '.join(title)
+    count = 0
+    msg = 'P2P-CI/1.0 404 NOT FOUND'
+    if len(active_RFCs) > 0:
+        msg = 'P2P-CI/1.0 200 OK\n'
+        for active_RFC in active_RFCs:
+            if active_RFC.rfc_number == rfc_number and active_RFC.rfc_title==title :
+                msg += 'RFC '+str(active_RFC.rfc_number)+' '+str(active_RFC.rfc_title)+' '+str(active_RFC.active_peer.hostname)+' '+str(active_RFC.active_peer.upload_port)
+                count += 1;
+    msg = add_padding(msg)
+    peer_socket.send(msg)
 
-def deletePeer(new_response,peerSocket):
-    split_response=new_response.split();
-    
-    hostName = split_response[4]
-    uploadPort = split_response[6]
-   
-    for activeRFC in activeRFCs:
-        if activeRFC.activePeer.hostName == hostName and activeRFC.activePeer.uploadPort== uploadPort:
-            activeRFCs.remove(activeRFC)
-        
+def list(peer_socket):
+    msg = 'P2P-CI/1.0 404 NOT FOUND'
+    if len(active_RFCs) > 0:
+        count = 0
+        msg = 'P2P-CI/1.0 200 OK \n'
+        for active_RFC in active_RFCs:
+           msg += str(active_RFC)+ '\n'
+           count += 1
+    msg = add_padding(msg)
+    peer_socket.send(msg)
 
-    for activePeer in activePeers:
-        if activePeer.hostName == hostName and activePeer.uploadPort == uploadPort:
-            activePeers.remove(activePeer)
-
-    message = 'P2P-CI/1.0 200 OK \n'
-    peerMessageResponse = concat_message(message)
-    peerSocket.send(peerMessageResponse)
-    
+def deletePeer(response,peer_socket):
+    arr = response.split(' ');
+    hostname = arr[4]
+    upload_port = arr[6]
+    for active_RFC in active_RFCs:
+        if active_RFC.active_peer.hostname == hostname and active_RFC.active_peer.upload_port== upload_port:
+            active_RFCs.remove(active_RFC)
+    for active_peer in active_peers:
+        if active_peer.hostname == hostname and active_peer.upload_port == upload_port:
+            active_peers.remove(active_peer)
+    msg = 'P2P-CI/1.0 200 OK \n'
+    msg = add_padding(msg)
+    peer_socket.send(msg)
